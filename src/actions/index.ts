ecc909dfcee844ae5df1384196aae01566137237
AwlCoke <virulentia@gmail.com>
2020-10-07 14:43:32 +0300

    error indicator scss

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,7 +2,6 @@
 import { TicketModel } from '../models/ticket.model';
 
 const ticketsLoaded = (tickets: Array<TicketModel>) => {
-    console.log('tickets loaded');
     return {
         type: 'TICKETS_LOADED',
         payload: tickets,
